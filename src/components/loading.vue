3ebc6885e45c34358842c4ea3c9248485c6576ea
sakurayeah <katrina136@163.com>
2019-03-27 16:04:44 +0800

    loading

---
+++ src/components/loading.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="csshub-loading">
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+</template>
+
+<script>
+import "./loading.less";
+</script>
